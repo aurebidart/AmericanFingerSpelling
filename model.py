ace1f7ebe3f7840a7199e7b795df63c9ef75e511
aurebidart <aure.bidart@gmail.com>
2023-06-01 10:42:50 -0300

    new model

--- model.py
+++ model.py
@@ -1,62 +1,39 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Embedding, LSTM, Attention, Dense, Conv1D, GlobalMaxPooling1D
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.callbacks import Callback
 
-# Definición de la arquitectura de la red
-def build_model(vocab_size, embedding_dim, hidden_units):
-    # Capa de entrada
-    inputs = Input(shape=(None,))
-    embedded = Embedding(vocab_size, embedding_dim)(inputs)
-    
-    # Capa recurrente
-    lstm_output = LSTM(hidden_units, return_sequences=True)(embedded)
-    
-    # Capa de atención
-    attention = Attention()([lstm_output, lstm_output])
-    
-    # Capa de decodificación
-    decoded = Dense(vocab_size, activation='softmax')(attention)
-    
-    # Construcción del modelo
-    model = tf.keras.Model(inputs=inputs, outputs=decoded)
-    return model
+class NeuralNetwork(tf.keras.Model):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
 
+        # Input layer
+        self.input_layer = tf.keras.layers.Input(shape=(126,), dtype=tf.float32)
 
-class MiRedNeuronal:
-    def __init__(self, num_frames, num_parametros, num_caracteres):
-        self.num_frames = num_frames
-        self.num_parametros = num_parametros
-        self.num_caracteres = num_caracteres
-        self.model = self._construir_modelo()
+        # Two convolutional layers
+        self.conv1 = tf.keras.layers.Conv1D(filters=128, kernel_size=3, strides=1, padding='same')(self.input_layer)
+        self.conv2 = tf.keras.layers.Conv1D(filters=64, kernel_size=3, strides=1, padding='same')(self.conv1)
 
-    def _construir_modelo(self):
-        model = Sequential()
-        model.add(Conv1D(filters=32, kernel_size=3, activation='relu', input_shape=(self.num_frames, self.num_parametros)))
-        model.add(LSTM(units=64, return_sequences=True))
-        model.add(GlobalMaxPooling1D())  # Capa de pooling para reducir la secuencia a un solo valor
-        model.add(Dense(31))  # Capa Dense con salida de tamaño 1
-        #model.add(Dense(self.num_caracteres, activation='softmax'))
-        #model.add(Dense(self.num_caracteres))
-        model.compile(loss='categorical_crossentropy', optimizer='adam')
-        return model
+        # Dropout layer
+        self.dropout = tf.keras.layers.Dropout(0.2)(self.conv2)
 
-    def resumen_modelo(self):
-        self.model.summary()
+        # Output layer
+        self.output_layer = tf.keras.layers.Dense(59, activation='softmax')(self.dropout)
 
-    def entrenar(self, x_train, y_train, epochs, batch_size):
-        # Definir el callback personalizado para seguir el progreso del entrenamiento
-        class ProgresoEntrenamientoCallback(Callback):
-            def on_epoch_end(self, epoch, logs=None):
-                # Imprimir métricas o información adicional después de cada época
-                print(f"Época {epoch+1}/{epochs} - Pérdida: {logs['loss']}")
+    def call(self, inputs):
+        return self.output_layer(inputs)
 
-        # Crear instancia del callback personalizado
-        progreso_callback = ProgresoEntrenamientoCallback()
+    def train(self, x_train, y_train, epochs=10, batch_size=128):
+        loss_fn = tf.keras.losses.CategoricalCrossentropy()
+        optimizer = tf.keras.optimizers.Adam()
 
-        # Entrenar el modelo con el callback personalizado
-        self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, callbacks=[progreso_callback])
+        for epoch in range(epochs):
+            for batch_x, batch_y in tf.keras.utils.data.Dataset.from_tensor_slices((x_train, y_train)).batch(batch_size):
+                loss = loss_fn(self(batch_x), batch_y)
+                optimizer.minimize(loss)
 
-    def evaluar(self, x_test, y_test):
-        loss = self.model.evaluate(x_test, y_test)
-        print(f"Pérdida en el conjunto de prueba: {loss}")
+    def predict(self, x_test):
+        return tf.argmax(self(x_test), axis=1)
+
+    def save(self, path):
+        tf.keras.models.save_model(self, path)
+
+    def load(self, path):
+        self = tf.keras.models.load_model(path)
